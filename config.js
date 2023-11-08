1700564054a698520aa1c069183c00f2ad8e9d08
Altrickz <150296394+Altrickz@users.noreply.github.com>
2023-11-08 22:12:37 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://films.pirobarbar2.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YzNjM2FjMjRkNmU0ZDNiZjM2MWY5ODlhZTc3MmQ0NSIsInN1YiI6IjY1NGJmNjQ4NGYzM2FkMDExZTAzNWZkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PQqluuwUFpg2FQnAo6ckBu3TurzVf3rWeoMS_fiZupo"
+};
